1270ebe009005e195c2b785dcf06ba8b45085f18
Marcos Lohmann <marcos.lohmann@gmail.com>
2022-06-27 18:54:42 -0300

    Update xhrUtil.js

--- web/js/xhrUtil.js
+++ web/js/xhrUtil.js
@@ -1,3 +1,13 @@
+/********************************************
+   JSON file functions     
+ ********************************************/
+
+/* -----------------------------------------
+_xhrGetJSON("path/to/file.json",function(err,res){
+    if(err) doSomethingWith(err);
+    if(res) doSomethingWith(res);
+})
+*/
 var _xhrGetJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
@@ -13,7 +23,45 @@ var _xhrGetJSON = function(url, callback) {
     xhr.send();
 };
 
-var createTag = function(params){
+/* -----------------------------------------
+jsonGet({
+    url: "path/to/file.json",
+    success: function(res){
+        doSomethingWith(res);
+    },
+    error: function(data){
+        doSomethingWith(err);
+    }
+})
+*/
+var jsonGet = function(parms){
+   _xhrGetJSON(parms.url, function(err,res){
+     if (err !== null) {
+       if(!parms.error) console.log(err);
+       else parms.error(err)
+     } else {
+       if(!parms.success) console.log(res);
+       else parms.success(res);
+     }      
+   });
+}
+
+/********************************************
+   Dynamic HTML functions     
+ ********************************************/
+
+/* -----------------------------------------
+let myNewElement = createNewElement({
+    tag: "div || span || img || input || a || whatever",
+    parent: parentElement || parentElement.id || parentElement.class,
+    class: "whatever css classnames",
+    id: "someUniqueId",
+    src: "source/if/applicable",
+    type: "type/if/applicable",
+    anyother: "anyOtherAttributeValue"
+})
+*/
+var createNewElement = function(params){
     var el = document.createElement(params.tag);
     if(!!params.parent) {
         if(typeof params.parent === 'object') params.parent.append(el);
@@ -26,14 +74,3 @@ var createTag = function(params){
     return el;
 }
 
-var jsonGet = function(parms){
-   _xhrGetJSON(parms.url, function(err,res){
-     if (err !== null) {
-       if(!parms.error) console.log(err);
-       else parms.error(err)
-     } else {
-       if(!parms.success) console.log(res);
-       else parms.success(res);
-     }      
-   });
-}
